ce15b0986515409f889e3a98833444d0a05101cf
BEN ARIEL FRANÇA MARTINS <fadomarshemello@gmail.com>
2024-11-19 15:22:36 -0300

    rotas para acessar um post ou todos os posts

--- Server.js
+++ Server.js
@@ -1,10 +1,47 @@
 import express from 'express';
 
+const posts = [
+    {
+        id: 1,
+        descricao: 'foto de teste',
+        url: 'https://placecats.com/millie/300/200'
+    },
+    {
+        id:2,
+        descricao: 'paisagem montanhosa',
+        url: 'https://source.unsplash.com/random/300x200/?mountain'
+    },
+    {
+        id:3,
+        descricao: 'comida deliciosa',
+        url: 'https://source.unsplash.com/random/300x200/?food'
+    },
+    {
+        id:4,
+        descricao: 'cachorro fofo',
+        url: 'https://source.unsplash.com/random/300x200/?dog'
+    },
+    {
+        id:5,
+        descricao: 'cidade à noite',
+        url: 'https://source.unsplash.com/random/300x200/?city,night'
+    },
+];
+
 const app = express();
+app.use(express.json());
 app.listen(3000, () => {
     console.log('Server escutando na porta 3000');
 });
 
-app.get("/api", (req, res) => {
-    res.send('rota inicial');
+const BuscarPostPorId = (id) => {
+    return posts.findIndex(post => post.id === Number(id));
+};
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
+});
+app.get("/posts/:id", (req, res) => {
+    const index = BuscarPostPorId(req.params.id);
+    console.log(index);
+    res.status(200).json(posts[index]);
 });
