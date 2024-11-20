e8cb1a9af7ccf0aa6aa6114b0cd61d6d7a1fbb61
BEN ARIEL FRANÇA MARTINS <fadomarshemello@gmail.com>
2024-11-20 17:07:09 -0300

    conexão com o banco de dados

--- Server.js
+++ Server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import conectarAoBanco from './src/config/dbConfig.js';
 
 const posts = [
     {
@@ -28,20 +29,31 @@ const posts = [
     },
 ];
 
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
+
+async function getPosts () {
+    const db = conexao.db("instaBytes");
+    const colecao = db.collection("posts");
+    return colecao.find().toArray();
+}
 const app = express();
 app.use(express.json());
 app.listen(3000, () => {
     console.log('Server escutando na porta 3000');
 });
 
+app.get("/posts", async (req, res) => {
+    const posts = await getPosts();
+    res.status(200).json(posts);
+});
+
 const BuscarPostPorId = (id) => {
     return posts.findIndex(post => post.id === Number(id));
 };
-app.get("/posts", (req, res) => {
-    res.status(200).json(posts);
-});
+
 app.get("/posts/:id", (req, res) => {
     const index = BuscarPostPorId(req.params.id);
     console.log(index);
     res.status(200).json(posts[index]);
 });
+
